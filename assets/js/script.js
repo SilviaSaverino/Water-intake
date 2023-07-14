9235df37066a1cb26c74c02582577aafaa70a28c
Silvia Saverino <silviasave89@gmail.com>
2023-07-14 23:39:36 +0000

    add conditional statement to toggle the last full cup

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,19 @@ smallCup.forEach((cup, idx) => {
 });
 
 function fillUpCups(idx) {
+  if (
+    smallCup[idx].classList.contains("full") &&
+    !smallCup[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
   smallCup.forEach((cup, idx2) => {
     if (idx2 <= idx) {
       cup.classList.add("full");
     } else {
       cup.classList.remove("full");
     }
-  });
+  }); 
 }
+
