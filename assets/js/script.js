96a7ce64edd51be313ca199147313eb2b9f333dd
Silvia Saverino <silviasave89@gmail.com>
2023-07-14 23:26:25 +0000

    add functionality to fill up cups

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 const waterEffect = document.getElementById("bounce");
-const main = document.getElementById("main")
+const main = document.getElementById("main");
+
+const smallCup = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liter");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
 
 waterEffect.addEventListener("click", () => {
   main.classList.add("show");
 });
+
+smallCup.forEach((cup, idx) => {
+  console.log(idx);
+  cup.addEventListener("click", () => fillUpCups(idx));
+});
+
+function fillUpCups(idx) {
+  smallCup.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
